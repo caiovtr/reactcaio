cac06735c5d9e6fa701d100fff8b43c20340453a
caiovtr <caiovitorandraderosa@gmail.com>
2023-06-30 10:33:49 -0300

    react

---
+++ src/pages/Bonus/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+import TruckDif from "../../assets/caminhaodif.jpg"
+
+export function Bonus() {
+  return (
+    <S.Section>
+      <img src={TruckDif} alt="Caminhão Diferente" />
+    </S.Section>
+  )
+}
